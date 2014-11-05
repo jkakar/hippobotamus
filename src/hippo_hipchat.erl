18f0601ab0ecb201dc05fdc86582629aecc63e05
Jamu Kakar <jkakar@kakar.ca>
2014-11-04 21:36:28 -0800

    Make the HipChat room configurable.

--- src/hippo_hipchat.erl
+++ src/hippo_hipchat.erl
@@ -1,9 +1,9 @@
 -module(hippo_hipchat).
 
--export([send_message/2]).
+-export([send_message/3]).
 
-send_message(Token, Message) ->
-    URL = "https://api.hipchat.com/v2/room/Orchestration/notification",
+send_message(Token, Room, Message) ->
+    URL = "https://api.hipchat.com/v2/room/" ++ Room ++ "/notification",
     Headers = [{"Authorization", "Bearer " ++ Token}],
     ContentType = "application/json",
     Body = "{\"color\": \"purple\", \"message\": \"" ++ Message ++ "\"}",
